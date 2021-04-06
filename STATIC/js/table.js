9739c64c61f832bed21912597ae611b8b8e2bb68
gabrielavalos <gabby.avalos16@gmail.com>
2021-04-06 16:15:47 -0500

    daily import table complete

--- STATIC/js/table.js
+++ STATIC/js/table.js
@@ -2,17 +2,22 @@
 // import the data from mostRecentProduction.json
 
 
-
+// read in yesterdays production data for all Hawkwood sites
 d3.json("/DATA/recent.json").then(function(data) {
+  // print the array to the console
   console.log(data);
-
-  data.forEach((siteProduction) => {
-   // console.log(siteProduction.Site_Name);
-   let row = tbody.append("tr");
-   Object.values(siteProduction).forEach((val) => {
+  // loop thrugh the array
+  data.forEach((well) => {
+    //print each object in the array to the console
+    console.log(well);
+    // Reference the HTML table using d3
+    var tbody = d3.select("tbody");
+    // for each well add a row
+    let row = tbody.append("tr");
+    // loop through each value to add a cell for each of it
+    Object.values(well).forEach((val) => {
       let cell = row.append("td");
       cell.text(val);
-   });
-  })
-});
-
+    })
+     }); // closing forEach
+   }); //closing d3
