e6c60d15857c45b816b038e69399c2a2ca0a6f3a
gabrielavalos <gabby.avalos16@gmail.com>
2021-04-07 17:14:12 -0500

    filter working

--- STATIC/js/table.js
+++ STATIC/js/table.js
@@ -1,20 +1,20 @@
 // a way to read in all production data from a .js file
 const tableData = recentImport;
-// console.log(tableData);
+console.log(tableData);
 //global declaration of tbody, since it will be accessed in and out of functions
 var tbody = d3.select("tbody");
 
 // CODE FOR MOST RECENT PRODUCTION IN TABULAR FORM
 // function to import the data from recent.json, saved in this folder from Jupyter Notebook
-function buildTable(data) {
+function buildTable(tableData) {
   //clear table...is this necessary?
   tbody.html("");
   // read in yesterdays production data for all Hawkwood sites
-  d3.json("/DATA/recent.json").then(function(data) {
+  //d3.json("/DATA/recent.json").then(function(data) { //this is giving me an issue !!!!!!
     // print the array to the console 
-    console.log(data);
+    //console.log(data);
     // loop thrugh the array
-    data.forEach((well) => {
+    tableData.forEach((well) => {
       //print each object in the array to the console
       console.log(well);
       // for each well add a row to the tbody
@@ -25,19 +25,19 @@ function buildTable(data) {
         cell.text(val);
       })
     }); // closing forEach
-  }); //closing d3
-};
+  }; //closing d3
+
 
 function handleClick() {
   // the value entered in the sitename filter becomes the value for the siteName variable
-  let requestedSiteName = d3.select("#sitename").property("value");
+  let requestedSiteName = d3.select("#siteName").property("value");
   // set data be filtered to imported data (the data ready to be filtered)
   let filteredData = tableData;
-  if (siteName) {
-    filteredData = filteredData.filter (row => row.sitename === siteName);
-    console.log(filteredData); //check to see what this looks like because i dont understand it 
+  if (requestedSiteName) {
+    filteredData = filteredData.filter(row => row.Site_Name == requestedSiteName)
+    console.log(filteredData); //check to see what this looks like because i dont understand it
   };
-  //built table using the filtedData varible
+  //build table using the filteredData variable
   buildTable(filteredData);
 };
 
