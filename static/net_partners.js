9027bb8d0a87ab7ae04b67416f73aed9683b0e4e
gabrielavalos <gabby.avalos16@gmail.com>
2021-05-10 17:08:46 -0500

    options functions

--- static/net_partners.js
+++ static/net_partners.js
@@ -1,4 +1,4 @@
-function createOptions() {
+function createWellOptions() {
     var wellSelector = d3.select("#well-partners");
     var partnerSelector = d3.select("#partner-name");
 
@@ -12,13 +12,29 @@ function createOptions() {
         })
     })
 
+    let options = document.getElementById('well-partners').selectedOptions
+    let values = Array.from(options).map(({ value }) => value);
 
-    
-    d3.json('./static/net_interests.json').then((data) => {
-    console.log(data);
-    var wellOptions = data
+
+
+    d3.json('./static/net_interests.json').then((well) => {
+    //console.log(well);
+    var wellOptions = new Array()
+    // IF [1] IS IN VALUES, THEN wellOptions IS[3]
+
+    well.forEach((site) => {
+        if (values.includes(site[0])) {
+          wellOptions = site[3]
+            console.log(wellOptions);
+        }
+      })
 
 }) //END OF d3
 }; //END OF createOptions() 
 
-createOptions()
+createWellOptions()
+
+//function createPartnerOptions()
+//based on selected wells
+
+//d3.selectAll('well-partners').on('change', createPartnerOptions);
